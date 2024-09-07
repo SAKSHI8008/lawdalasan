368026e09652f2e460a434f4e05d77e3fbe2b237
Shravankumarworld <sutharshravan42@gmail.com>
2024-09-07 18:00:00 +0530

    Add files via upload

---
+++ happy.py
@@ -0,0 +1,9 @@
+import os
+
+# Environment variables set karo
+os.environ['API_ID'] = '25120174'
+os.environ['API_HASH'] = '89a10ea368b634194752731a7c405e30'
+os.environ['BOT_TOKEN'] = '7228705691:AAEdhGPKBiioAiefzLEOsfIaVt6H2byZyBw'
+
+# Apne original script ka correct path yaha provide karo
+os.system('python3 modules/main.py')
