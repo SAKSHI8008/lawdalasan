3efec9746ac66bbf06a1e7c8e7c7ff02e509fad8
Shravankumarworld <sutharshravan42@gmail.com>
2024-09-07 18:04:06 +0530

    Add files via upload

---
+++ modules/utils.py
@@ -0,0 +1,102 @@
+import time
+import math
+import os
+import requests
+from pyrogram.errors import FloodWait
+from datetime import datetime, timedelta
+
+class Timer:
+    def __init__(self, time_between=5):
+        self.start_time = time.time()
+        self.time_between = time_between
+
+    def can_send(self):
+        if time.time() > (self.start_time + self.time_between):
+            self.start_time = time.time()
+            return True
+        return False
+
+def hrb(value, digits=2, delim="", postfix=""):
+    """Return a human-readable file size."""
+    if value is None:
+        return None
+    chosen_unit = "B"
+    for unit in ("KiB", "MiB", "GiB", "TiB"):
+        if value > 1000:
+            value /= 1024
+            chosen_unit = unit
+        else:
+            break
+    return f"{value:.{digits}f}" + delim + chosen_unit + postfix
+
+def hrt(seconds, precision=0):
+    """Return a human-readable time delta as a string."""
+    pieces = []
+    value = timedelta(seconds=seconds)
+    
+    if value.days:
+        pieces.append(f"{value.days}d")
+
+    seconds = value.seconds
+
+    if seconds >= 3600:
+        hours = int(seconds / 3600)
+        pieces.append(f"{hours}h")
+        seconds -= hours * 3600
+
+    if seconds >= 60:
+        minutes = int(seconds / 60)
+        pieces.append(f"{minutes}m")
+        seconds -= minutes * 60
+
+    if seconds > 0 or not pieces:
+        pieces.append(f"{seconds}s")
+
+    if not precision:
+        return "".join(pieces)
+
+    return "".join(pieces[:precision])
+
+def download_file(url, local_filename):
+    """Download a file from a URL."""
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(local_filename, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+        print(f"File downloaded successfully: {local_filename}")
+    else:
+        print(f"Failed to download file. Status code: {response.status_code}")
+
+async def progress_bar(current, total, reply, start):
+    """Update progress bar during file upload."""
+    if timer.can_send():
+        now = time.time()
+        diff = now - start
+        if diff < 1:
+            return
+        else:
+            perc = f"{current * 100 / total:.1f}%"
+            elapsed_time = round(diff)
+            speed = current / elapsed_time
+            remaining_bytes = total - current
+            if speed > 0:
+                eta_seconds = remaining_bytes / speed
+                eta = hrt(eta_seconds, precision=1)
+            else:
+                eta = "-"
+            sp = str(hrb(speed)) + "/s"
+            tot = hrb(total)
+            cur = hrb(current)
+            bar_length = 11
+            completed_length = int(current * bar_length / total)
+            remaining_length = bar_length - completed_length
+            progress_bar = "◆" * completed_length + "◇" * remaining_length
+            
+            try:
+                await reply.edit(f'\n `╭─⌯══⟰ 𝐔𝐩𝐥𝐨𝐝𝐢𝐧𝐠 ⟰══⌯──★ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n`├𝐁𝐲 » jagdeep\n╰─══ ✪ @jagdeep_56 ✪ ══─★\n') 
+            except FloodWait as e:
+                time.sleep(e.x)
+
+# Timer instance
+timer = Timer()
